97bb7898464bac17cf6be040abc97cc15b04eb42
Bhanu <bmani@allstate.com>
2018-05-09 11:34:36 -0500

    Test commit

--- src/main/java/com/xyz/contactapi/ContactApiApplication.java
+++ src/main/java/com/xyz/contactapi/ContactApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactApiApplication {
 
+	//Test 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactApiApplication.class, args);
 	}
