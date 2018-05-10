88aae4cdcc68d3f266d869ddac9fb0a65ffcbb5d
Maddy Mangipudi <mangipudi@gmail.com>
2018-05-10 11:34:55 +0800

    updates

--- src/main/java/com/xyz/contactapi/dto/entity/Contact.java
+++ src/main/java/com/xyz/contactapi/dto/entity/Contact.java
@@ -1,18 +1,28 @@
 package com.xyz.contactapi.dto.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import java.util.List;
 
-@Entity()
+@Entity
 @Table(name = "contact")
+@NoArgsConstructor
 public class Contact {
 
     @Column(nullable = false)
     @Id
-    public long id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
 }
